bcdcf34272261936de995425106466b4246b83bb
volody <groundsoft@hotmail.com>
2024-05-19 10:29:49 -0400

    update to aws v3

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 const bodyParser = require("body-parser");
 
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -17,16 +20,27 @@ const resolvers = {
   Query: {
     hello: () => "Hello world!",
     getSignedUploadURL: async (_, { filename, filetype }) => {
+      const s3Client = new S3Client({
+        region: "YOUR_REGION",
+        credentials: {
+          accessKeyId: "YOUR_ACCESS_KEY_ID",
+          secretAccessKey: "YOUR_SECRET_ACCESS_KEY",
+        },
+      });
+
       const s3Params = {
         Bucket: "YOUR_BUCKET_NAME",
         Key: filename,
-        Expires: 60, // the URL will expire in 60 seconds
         ContentType: filetype,
         ACL: "public-read", // or another ACL according to your needs
       };
 
+      const command = new PutObjectCommand(s3Params);
+
       try {
-        const signedUrl = await s3.getSignedUrlPromise("putObject", s3Params);
+        const signedUrl = await getSignedUrl(s3Client, command, {
+          expiresIn: 60,
+        });
         return signedUrl;
       } catch (err) {
         console.error(err);
@@ -35,16 +49,6 @@ const resolvers = {
     },
   },
 };
-
-const AWS = require("aws-sdk");
-
-AWS.config.update({
-  accessKeyId: "YOUR_ACCESS_KEY_ID",
-  secretAccessKey: "YOUR_SECRET_ACCESS_KEY",
-  region: "YOUR_REGION",
-});
-
-const s3 = new AWS.S3();
 
 // Create an instance of ApolloServer
 const server = new ApolloServer({ typeDefs, resolvers });
